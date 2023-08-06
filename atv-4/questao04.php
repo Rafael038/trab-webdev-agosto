0675caa15b070e8f31259d5373239edaf723cfa7
EduVMM <eduardoveloso2005@gmail.com>
2023-08-05 23:17:40 -0300

    finalizando arquivo questao 04

--- atv-4/questao04.php
+++ atv-4/questao04.php
@@ -17,7 +17,70 @@
     </header>
 
     <main>
-    
+    <h2>Trabalho: Questão 4</h2>
+
+<form action="" method="get" id="formulario-questoes">
+    <fieldset>
+        <legend>Parâmetros</legend>
+        <label for="aporteIni">Aporte inicial (R$):</label>
+        <input type="number" class="input-elementos" name="aporteIni" id="aporteIni" min="1" max="999999.99" step="0.01" value="500">
+        <label> [até R$999.999,99]</label>
+        <br>
+
+        <label for="periodo">Período (meses):</label>
+        <input type="number" class="input-elementos" name="periodo" id="periodo" min="1" max="480" value="12">
+        <label> [de 1 a 480 meses]</label>
+        <br>
+
+        <label for="rendimentoMen">Rendimento mensal (%):</label>
+        <input type="number" class="input-elementos" name="rendimentoMen" id="rendimentoMen" min="0.1" max="20" step="0.01" value="0.7">
+        <label> [até 20%]</label>
+        <br>
+
+
+        <label for="aporteMen">Aporte mensal:</label>
+        <input type="number" class="input-elementos" name="aporteMen" id="aporteMen" min="1" max="99999999.99" value="350">
+        <label> [até R$999.999,99]</label>
+        <br>
+
+        <input type="submit" class="input-botao" value="Processar">
+    </fieldset>
+
+    <?php
+    function calcularRendimento($aporteIni, $periodo, $rendimentoMen, $aporteMen)
+    {
+        $total = $aporteIni;
+        $dados = array();
+        for ($i = 1; $i <= $periodo; $i++) {
+            $aporte = ($i == 1) ? 0 : $aporteMen;
+            $rendimento = ($total + $aporte) * ($rendimentoMen / 100);
+            $total += $aporte + $rendimento;
+            $dados[] = array('mes' => $i, 'valor_inicial' => $aporteIni, 'aporte_mensal' => $aporte, 'rendimento' => $rendimento, 'total' => $total);
+        }
+        return $dados;
+    }
+
+    $dados = calcularRendimento($aporteIni, $periodo, $rendimentoMen, $aporteMen);
+    ?>
+
+    <table>
+        <tr>
+            <th>Mês</th>
+            <th>Valor Inicial (R$)</th>
+            <th>Aporte (R$)</th>
+            <th>Rendimento (R$)</th>
+            <th>Total (R$)</th>
+        </tr>
+        <?php foreach ($dados as $linha) : ?>
+            <tr>
+                <td><?php echo $linha['mes']; ?></td>
+                <td><?php echo number_format($linha['valor_inicial'], 2, ',', '.'); ?></td>
+                <td><?php echo number_format($linha['aporte_mensal'], 2, ',', '.'); ?></td>
+                <td><?php echo number_format($linha['rendimento'], 2, ',', '.'); ?></td>
+                <td><?php echo number_format($linha['total'], 2, ',', '.'); ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
     </main>
 
     <p><a href="index.php">Retornar a Página Inicial</a></p>
