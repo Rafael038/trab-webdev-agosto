9dc3eefe26015c1aecd89c55db13636192032d3f
EduVMM <eduardoveloso2005@gmail.com>
2023-08-05 23:16:58 -0300

    criacao arquivo questao 04

--- atv-4/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-$PAGE_NAME = 'Questão 4';
-include_once '../base/header.php'; ?>
-
-
-<?php include_once '../base/footer.php'; ?>
---
+++ atv-4/questao04.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trabalho: Questão 4</title>
+    <link rel="stylesheet" href="questao04.css">
+    
+</head>
+<html>
+
+<body>
+    <header>
+        <h1>Desenvolvimento Web</h1>
+    </header>
+
+    <main>
+    
+    </main>
+
+    <p><a href="index.php">Retornar a Página Inicial</a></p>
+
+    <footer>
+    <p> Eduardo Veloso/Rafael Barros - &copy; 2023</p>   
+    </footer>
+</body>
+
+</html>
