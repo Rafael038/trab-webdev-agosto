64a31147536ccf227fa63760023e5f38bd0c5650
EduVMM <eduardoveloso2005@gmail.com>
2023-08-05 23:09:41 -0300

    criacao  questao 03 do forms

---
+++ atv-3/questao03-4.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Questão 3</title>
+    <link rel="stylesheet" href="questao03.css">
+    <link rel="stylesheet" href="global.css">
+   
+</head>
+
+<body>
+    <header>
+        <h1>Desenvolvimento Web</h1>
+    </header>
+    <main>
+        
+    </main>
+    <footer>
+    <p> Eduardo Veloso/Rafael Barros - &copy; 2023</p>
+    </footer>
+</body>
+
+</html>
