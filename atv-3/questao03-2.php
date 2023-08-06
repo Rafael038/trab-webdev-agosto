e2cbd8b41b15c35d7f021c72d087935935ed0931
EduVMM <eduardoveloso2005@gmail.com>
2023-08-05 23:03:51 -0300

    terminando questao 03

--- atv-3/questao03-2.php
+++ atv-3/questao03-2.php
@@ -11,11 +11,49 @@
 </head>
 
 <body>
+
+<?php
+        $r1 = isset($_GET["r1"]) ? $_GET["r1"] : 0;
+?>
     <header>
         <h1>Desenvolvimento Web</h1>
     </header>
     <main>
-       
+    <div class="questoes">
+            <h2>Trabalho: Questão 3</h2>
+
+            <div class="perguntas">
+                <h3>Q2: Lorem ipsum dolor sit amet consectetur adipiscing elit?</h3>
+                    <form id="questao" action="questao03-1.php" method="get">
+                        <div class="resposta">
+                            <ol type="A">
+                                <a href="questao03-3.php?r1=<?php echo $r1 ?>&r2=1">
+                                    <li>Sed do eiusmod tempor.</li>
+                                </a>
+                                <a id="resp" href="questao03-3.php?r1=<?php echo $r1 ?>&r2=2">
+                                    <li>Incididunt ut labore.</li>
+                                </a>
+                                <a href="questao03-3.php?r1=<?php echo $r1 ?>&r2=3">
+                                    <li>Et dolore magna aliqua.</li>
+                                </a>
+                                <a href="questao03-3.php?r1=<?php echo $r1 ?>&r2=4">
+                                    <li>Enim nunc faucibus.</li>
+                                </a>
+                                <a href="questao03-3.php?r1=<?php echo $r1 ?>&r2=5">
+                                    <li>Pellentesque sit amet.</li>
+                                </a>
+                            </ol>
+                    </form>
+            </div>
+        </div>
+
+
+        </ol>
+
+        <a href="questao03-1.php">Cancelar<a>
+        <br>
+        <a href="index.php">Retornar a Página Inicial</a>
+        </div>
     </main>
     <footer>
     <p> Eduardo Veloso/Rafael Barros - &copy; 2023</p>
