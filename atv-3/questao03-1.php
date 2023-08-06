350506b80eb1ca04a2c222f8eaeb3266376f2788
EduVMM <eduardoveloso2005@gmail.com>
2023-08-05 23:00:25 -0300

    finalizando primeira parte questao 03

---
+++ atv-3/questao03-1.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Questão 3</title>
+    <link rel="stylesheet" href="questao03.css">
+    <link rel="stylesheet" href="global.css">
+</head>
+
+<body>
+    <header>
+        <h1>Desenvolvimento Web</h1>
+    </header>
+    <main>
+    <div class="questoes">
+            <h2>Trabalho: Questão 3 </h2>
+
+            <div class="perguntas">
+                <h3>Q1: Lorem ipsum dolor sit amet consectetur adipiscing elit?</h3>
+                    <form id="questao" action="questao03-1.php" method="get">
+                        <div class="resposta">
+                            <ol type="A">
+                                <a id="resp" href="questao03-2.php?&r1=1">
+                                    <li>Sed do eiusmod tempor.</li>
+                                </a>
+                                <a href="questao03-2.php?&r1=2">
+                                    <li>Incididunt ut labore.</li>
+                                </a>
+                                <a href="questao03-2.php?&r1=3">
+                                    <li>Et dolore magna aliqua.</li>
+                                </a>
+                                <a href="questao03-2.php?&r1=4">
+                                    <li>Enim nunc faucibus.</li>
+                                </a>
+                                <a href="questao03-2.php?&r1=5">
+                                    <li>Pellentesque sit amet.</li>
+                                </a>
+                            </ol>
+                    </form>
+            </div>
+        </div>
+
+
+        <a href="index.php">Retornar a Página Inicial</a>
+        </div>
+    </main>
+    <footer>
+    <p> Eduardo Veloso/Rafael Barros - &copy; 2023</p>
+    </footer>
+</body>
+
+</html>
