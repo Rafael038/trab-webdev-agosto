37f12a958fcb3289c9861f300775e0b18582615d
EduVMM <eduardoveloso2005@gmail.com>
2023-08-05 23:11:59 -0300

    criacao arquivo resultado questao 03

---
+++ atv-3/questao03-resultado.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Questão 3</title>
+    <link rel="stylesheet" href="questao03.css">
+    <link rel="stylesheet" href="global.css">
+    <script src="https://kit.fontawesome.com/e595c28941.js" crossorigin="anonymous"></script>
+
+    
+</head>
+
+<body>
+    <header>
+        <h1>Desenvolvimento Web</h1>
+    </header>
+    <main>
+        
+    </main>
+    <footer>
+    <p> Eduardo Veloso/Rafael Barros - &copy; 2023</p>
+    </footer>
+</body>
+
+</html>
